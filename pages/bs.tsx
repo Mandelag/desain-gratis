e549e9cbf90421ffe781f7cace35337c50a17e9b
keenan.gebze <keenan.gebze@gmail.com>
2022-07-05 22:38:15 +0700

    trying create bottom sheet2

--- pages/bs.tsx
+++ pages/bs.tsx
@@ -18,7 +18,7 @@ const BottomSheetDemo = () => {
     
     return (
         <>
-        <main style={{height: '90vh', width: '100%', overflowY: 'scroll'}}>
+        <main style={{height: '80vh', width: '100%', overflowY: 'scroll'}}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus mattis rhoncus lectus sit amet malesuada. Aliquam ut posuere diam, varius consectetur nulla. Morbi bibendum elit in rutrum efficitur. Aliquam tempor, est eget tincidunt ullamcorper, erat nisi rutrum lorem, in varius risus enim vitae lorem. Ut sit amet mauris eu libero vulputate fringilla. Donec posuere turpis ut velit imperdiet, ultrices cursus leo pretium. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce sed feugiat diam.
             <br /><br /><br />
             Cras ante magna, tempus quis vehicula vitae, porta vel orci. Sed nec vestibulum odio, in varius velit. Nulla facilisi. Nam ullamcorper diam a massa venenatis ullamcorper. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla laoreet vestibulum ex, quis placerat risus porta eu. Donec at orci a quam egestas sodales vitae sed odio. In ante est, pharetra ac auctor nec, accumsan eu lacus. Duis malesuada, sem in tempus lacinia, ligula lectus dictum risus, id auctor lacus quam eu lorem. Aenean ornare libero lacus, a tempus tellus tincidunt eu. Nulla facilisi. Pellentesque porttitor vulputate efficitur. Cras placerat ante nec tortor pharetra, ac ultrices ligula convallis. Integer a nibh condimentum, tincidunt orci nec, malesuada libero. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc sodales vel nunc at dictum.
@@ -30,7 +30,7 @@ const BottomSheetDemo = () => {
             Pellentesque sit amet cursus lorem, ut pellentesque justo. Vestibulum quis laoreet metus. Proin non ante eget elit accumsan vestibulum. Cras sed cursus quam, in laoreet dui. Vivamus scelerisque ullamcorper leo sit amet posuere. Donec a risus faucibus, scelerisque mi ut, bibendum erat. Mauris lobortis euismod ipsum, nec efficitur dui gravida eu. Mauris eget lorem non dolor imperdiet mollis. In sit amet neque non lacus dictum aliquam id id diam. Ut hendrerit aliquet nibh, a laoreet diam ornare nec. Fusce scelerisque orci augue, nec aliquet felis sagittis ac. Mauris id nisl nec sapien ullamcorper blandit eget at felis. Aliquam at est convallis, dignissim lacus ac, elementum libero. Phasellus dictum dictum purus, laoreet malesuada ipsum mollis ut. Etiam pharetra facilisis commodo. Nam lacinia arcu volutpat feugiat tempor.
             <br /><br /><br />
         </main>
-        <BottomSheet>
+        <div style={{height: '20vh', width:'100%', backgroundColor: 'pink'}}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus mattis rhoncus lectus sit amet malesuada. Aliquam ut posuere diam, varius consectetur nulla. Morbi bibendum elit in rutrum efficitur. Aliquam tempor, est eget tincidunt ullamcorper, erat nisi rutrum lorem, in varius risus enim vitae lorem. Ut sit amet mauris eu libero vulputate fringilla. Donec posuere turpis ut velit imperdiet, ultrices cursus leo pretium. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce sed feugiat diam.
             <br /><br /><br />
             Cras ante magna, tempus quis vehicula vitae, porta vel orci. Sed nec vestibulum odio, in varius velit. Nulla facilisi. Nam ullamcorper diam a massa venenatis ullamcorper. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla laoreet vestibulum ex, quis placerat risus porta eu. Donec at orci a quam egestas sodales vitae sed odio. In ante est, pharetra ac auctor nec, accumsan eu lacus. Duis malesuada, sem in tempus lacinia, ligula lectus dictum risus, id auctor lacus quam eu lorem. Aenean ornare libero lacus, a tempus tellus tincidunt eu. Nulla facilisi. Pellentesque porttitor vulputate efficitur. Cras placerat ante nec tortor pharetra, ac ultrices ligula convallis. Integer a nibh condimentum, tincidunt orci nec, malesuada libero. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc sodales vel nunc at dictum.
@@ -41,7 +41,7 @@ const BottomSheetDemo = () => {
             <br /><br /><br />
             Pellentesque sit amet cursus lorem, ut pellentesque justo. Vestibulum quis laoreet metus. Proin non ante eget elit accumsan vestibulum. Cras sed cursus quam, in laoreet dui. Vivamus scelerisque ullamcorper leo sit amet posuere. Donec a risus faucibus, scelerisque mi ut, bibendum erat. Mauris lobortis euismod ipsum, nec efficitur dui gravida eu. Mauris eget lorem non dolor imperdiet mollis. In sit amet neque non lacus dictum aliquam id id diam. Ut hendrerit aliquet nibh, a laoreet diam ornare nec. Fusce scelerisque orci augue, nec aliquet felis sagittis ac. Mauris id nisl nec sapien ullamcorper blandit eget at felis. Aliquam at est convallis, dignissim lacus ac, elementum libero. Phasellus dictum dictum purus, laoreet malesuada ipsum mollis ut. Etiam pharetra facilisis commodo. Nam lacinia arcu volutpat feugiat tempor.
             <br /><br /><br />
-        </BottomSheet>
+        </div>
     </>
     );
 }; 
